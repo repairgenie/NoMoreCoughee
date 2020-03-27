e34aa4221b0ec6e73b0086dd4397973dc8a9444c
DevilMouse Software <pekinwebguy@gmail.com>
2020-03-27 12:34:12 -0500

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,29 +1,29 @@
 /*
- * Trump Filter - Content Script
- *
- * This is the primary JS file that manages the detection and filtration of Donald Trump from the web page.
+ * Coronavirus Filter - Content Script
+ * Based on Trump Filter
+ * This is the primary JS file that manages the detection and filtration of Coronavirus from the web page.
  */
 
 // Variables
-var regex = /Trump/i;
+var regex = /[covid\-19][coronavirus][pandemic]/i;
 var search = regex.exec(document.body.innerText);
 
-var selector = ":contains('Trump'), :contains('TRUMP'), :contains('trump')";
+var selector = ":contains('covid-19'), :contains('COVID-19'), :contains('coronavirus')";
 
 
 // Functions
 function filterMild() {
-	console.log("Filtering Trump with Mild filter...");
+	console.log("Filtering Coronavirus with Mild filter...");
 	return $(selector).filter("h1,h2,h3,h4,h5,p,span,li");
 }
 
 function filterDefault () {
-	console.log("Filtering Trump with Default filter...");
+	console.log("Filtering Coronavirus with Default filter...");
 	return $(selector).filter(":only-child").closest('div');
 }
 
 function filterVindictive() {
-	console.log("Filtering Trump with Vindictive filter...");
+	console.log("Filtering Coronavirus with Vindictive filter...");
 	return $(selector).filter(":not('body'):not('html')");
 }
 
@@ -47,15 +47,15 @@ function filterElements(elements) {
 
 // Implementation
 if (search) {
-   console.log("Donald Trump found on page! - Searching for elements...");
+   console.log("Coronavirus found on page! - Searching for elements...");
    chrome.storage.sync.get({
      filter: 'aggro',
    }, function(items) {
 	   console.log("Filter setting stored is: " + items.filter);
 	   elements = getElements(items.filter);
 	   filterElements(elements);
-	   chrome.runtime.sendMessage({method: "saveStats", trumps: elements.length}, function(response) {
-			  console.log("Logging " + elements.length + " trumps.");
+	   chrome.runtime.sendMessage({method: "saveStats", coronavirus: elements.length}, function(response) {
+			  console.log("Logging " + elements.length + " coronavirus.");
 		 });
 	 });
   chrome.runtime.sendMessage({}, function(response) {});
